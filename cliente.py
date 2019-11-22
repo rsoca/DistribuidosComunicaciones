3b498d7e758bc446ad687a578c8163265a0d9e1e
irr97 <56109061+irr97@users.noreply.github.com>
2019-11-22 12:18:11 +0100

    Add files via upload

---
+++ cliente.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import Ice
+Ice.loadSlice('trawlnet.ice')
+import TrawlNet
+
+'''
+Cliente
+'''
+
+class Cliente(Ice.Application):
+    '''
+    Client
+    '''
+    def run(self, argv):
+        '''
+        Run
+        '''
+        print('/////////////////////////////////////////////////////////////////')
+        print('// Bienvenido a la aplicacion para descargar musica de Youtube //')
+        print('/////////////////////////////////////////////////////////////////')
+        proxy = self.communicator().stringToProxy(argv[1])
+        orchestrator = TrawlNet.OrchestratorPrx.checkedCast(proxy)
+
+        if not orchestrator:
+            raise RuntimeError('Proxy no valido')
+
+        orchestrator.downloadTask(argv[2])
+        return 0
+
+sys.exit(Cliente().main(sys.argv))

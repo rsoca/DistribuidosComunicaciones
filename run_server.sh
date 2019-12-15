f1ded0e77f78f311ffb24037888183608ce22e62
rsoca <37048784+rsoca@users.noreply.github.com>
2019-12-15 02:05:17 +0100

    Add files via upload

--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
+#
 
-./downloader.py --Ice.Config=serv_downloader.config | tee down.out &
-sleep 4
-./orchestrator.py --Ice.Config=serv_orch.config "$(head -1 down.out)"
+PYTHON=python3
+
+DOWNLOADER_CONFIG=serv.config
+ORCHESTRATOR_CONFIG=$DOWNLOADER_CONFIG
+
+./run_icestorm.sh &
+
+PRX=$(tempfile)
+$PYTHON downloader.py --Ice.Config=$DOWNLOADER_CONFIG>$PRX &
+PID=$!
+
+# Dejamos arrancar al downloader
+sleep 1
+echo "Downloader: $(cat $PRX)"
+
+# Lanzamos el orchestrator
+$PYTHON orchestrator.py --Ice.Config=$ORCHESTRATOR_CONFIG "$(cat $PRX)"
+
+echo "Shoutting down..."
+kill -KILL $PID
+rm $PRX

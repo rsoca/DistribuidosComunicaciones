81eeccc60d7a0f36c8adb92ae8ac48bdc889d1b4
irr97 <56109061+irr97@users.noreply.github.com>
2020-01-20 22:27:18 +0100

    Add files via upload

--- transfer_factory.py
+++ transfer_factory.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+'''
+Transfer Factory
+'''
+
 import os
 import sys
 import binascii
-
 import Ice
-import IceGrid
-
+import IceGrid # pylint : disable=W0611
 Ice.loadSlice('trawlnet.ice')
-import TrawlNet
+import TrawlNet # pylint: disable=E0401,C0413
 
 
 APP_DIRECTORY = './'
@@ -17,16 +19,21 @@ DOWNLOADS_DIRECTORY = os.path.join(APP_DIRECTORY, 'downloads')
 
 
 class TransferI(TrawlNet.Transfer):
+    ''' TransferI '''
     def __init__(self, file_path):
+        ''' Constructor '''
         self.file_ = open(file_path, 'rb')
 
     def recv(self, size, current):
+        ''' Recv '''
         return str(binascii.b2a_base64(self.file_.read(size), newline=False))
 
     def close(self, current):
+        ''' Close '''
         self.file_.close()
 
     def destroy(self, current):
+        ''' Destroy '''
         try:
             current.adapter.remove(current.id)
             print('TRASFER DESTROYED', flush=True)
@@ -35,7 +42,9 @@ class TransferI(TrawlNet.Transfer):
 
 
 class TransferFactoryI(TrawlNet.TransferFactory):
+    ''' TransferFactory I '''
     def create(self, file_name, current):
+        ''' Create '''
         file_path = os.path.join(DOWNLOADS_DIRECTORY, file_name)
         servant = TransferI(file_path)
         proxy = current.adapter.addWithUUID(servant)
@@ -45,26 +54,32 @@ class TransferFactoryI(TrawlNet.TransferFactory):
 
 
 class Server(Ice.Application):
+    '''
+    Server
+    '''
     def run(self, args):
-        ic = self.communicator()
-        properties = ic.getProperties()
+        '''
+        run 
+        '''
+        broker = self.communicator()
+        properties = broker.getProperties()
 
         servant = TransferFactoryI()
-        adapter = ic.createObjectAdapter('TransferAdapter')
+        adapter = broker.createObjectAdapter('TransferAdapter')
         factory_id = properties.getProperty('TransferFactoryIdentity')
-        proxy = adapter.add(servant, ic.stringToIdentity(factory_id))
+        proxy = adapter.add(servant, broker.stringToIdentity(factory_id))
 
         print('{}'.format(proxy), flush=True)
 
         adapter.activate()
         self.shutdownOnInterrupt()
-        ic.waitForShutdown()
+        broker.waitForShutdown()
 
         return 0
 
 
 if __name__ == '__main__':
-    app = Server()
-    exit_status = app.main(sys.argv)
+    SERVER = Server()
+    exit_status = SERVER.main(sys.argv)
     sys.exit(exit_status)
 
